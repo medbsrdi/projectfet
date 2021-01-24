7b31ef2d65185cd666dbcbb95b0f759dcfc539c0
medbsrdi <66888805+medbsrdi@users.noreply.github.com>
2021-01-24 01:20:38 +0100

    uploading the project folder
    this actually will not work because of the data base but just for my friend to have some idea about my code

---
+++ myProject/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PF64</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    <!-- navigation bootstrap -->
+    <nav class="navbar navbar-info bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+            <img src="images/hacker.png" alt="" width="30" height="30" class="d-inline-block align-top">
+                PF64
+            </a>
+        </div>
+    </nav>
+</body>
+</html>
